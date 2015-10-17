a0b9672a4accd5ff5cc660218b7c4f74f67de0fc
hamham100 <victor.hamer@student.uclouvain.be>
2015-10-17 10:44:33 +0200

    tt

---
+++ sender.c
@@ -0,0 +1,23 @@
+#include "packet_interface.h"
+#include <zlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include "chatUDP/wait_for_client"
+#include "chatUDP/real_address"
+#include "chatUDP/create_socket"
+
+
+int main(int argc , char * argv[]){
+
+
+  char * adresse = argv[1];
+  int port = atoi(argv[2]);
+
+  struct 
+  
+
+
+
+
+}
